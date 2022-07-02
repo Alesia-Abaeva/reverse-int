f519dc411200078f12672afe0aeed0608889c45e
Alesia <alesia.litvinenko@gmail.com>
2022-07-02 23:00:13 +0300

    feat: revers int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
+    let numToString = n.toString()
+    let stringTonum = ''
+    if(numToString[0] === '-' ){ 
+        numToString = numToString.slice(1)
+
+    }
+     for(let i = numToString.length - 1; i >= 0; i--){
+        stringTonum = stringTonum + numToString[i]
+    } 
+    return Number(stringTonum)
   
 }
